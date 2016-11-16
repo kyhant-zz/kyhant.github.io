b2fc5d53026ce50f94f6c2e9a7a2b5ec6a629e58
Kyhan Turner <kyhanpt@gmail.com>
2016-11-15 20:45:58 -0800

    disable portfolio button if not available

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -15,6 +15,7 @@ class Portfolio extends Component {
 						description="Collaboration platform to match qualified developers with independent product owners."
 						stack="React.js, Node.js, Express, MongoDB, and Mongoose."
 						github='https://github.com/Git-Luv/GitLuv'
+						disabled=''
 						url='http://www.gitluv.io/' />
 					<PortfolioItem
 						img='../../style/images/bpc.jpg'
@@ -22,12 +23,14 @@ class Portfolio extends Component {
 						description="Re-implementation of a rock-paper-scissors app with chat, challenge and rematch features."
 						stack="React.js, Socket.io, Node.js, Express and MongoDB."
 						github='https://github.com/GoodNews-MKS-39/RockPaperScissors'
+						disabled=''
 						url='http://bearprincesscowboy.com/' />
 					<PortfolioItem
 						img='../../style/images/flashback.jpg'
 						title='Flashback'
 						description="Search any date back to the 1860's, explore the top headlines of the day in the New York Times."
 						stack="React.js, Bootstrap, New York Times API."
+						disabled='disabled'
 						github='https://github.com/kyhant/flashback' />
 					</div>
 			)
--- src/components/portfolio_item.js
+++ src/components/portfolio_item.js
@@ -5,36 +5,19 @@ class PortfolioItem extends Component {
 		super(props);
 	}
 	render(){
-		if(this.props.url){
 		return (
-		  <div className="col-sm-12 col-md-4">
-		    <div className="thumbnail">
-		      <img className="portfolio-img" src={this.props.img} alt="..."></img>
-		      <div className="caption">
-		        <h3>{this.props.title}</h3>
-		        <p>{this.props.description}</p>
-		        <p><strong>Developed with: </strong>{this.props.stack}</p>
-		        <p className='links'><a href={this.props.github} className="btn btn-default" target="blank" role="button"><i className="fa fa-lg fa-github"></i></a><a href={this.props.url} className="btn btn-primary" target="blank" role="button"><i className="fa fa-globe fa-lg"></i></a></p>
-		      </div>
-		    </div>
-			</div>
-		)}
-		else {
-			return (
-			  <div className="col-sm-12 col-md-4">
-			    <div className="thumbnail">
-			      <img className="portfolio-img" src={this.props.img} alt="..."></img>
-			      <div className="caption">
-			        <h3>{this.props.title}</h3>
-			        <p>{this.props.description}</p>
-			        <p><strong>Developed with: </strong>{this.props.stack}</p>
-			        <p className='links'><a href={this.props.github} className="btn btn-default" target="blank" role="button"><i className="fa fa-lg fa-github"></i></a></p>
-			      </div>
-			    </div>
-				</div>
-			)
-		}
-	}
+		 <div className="col-sm-12 col-md-4">
+	    <div className="thumbnail">
+	      <img className="portfolio-img" src={this.props.img} alt="..."></img>
+	      <div className="caption">
+	        <h3>{this.props.title}</h3>
+	        <p>{this.props.description}</p>
+	        <p><strong>Developed with: </strong>{this.props.stack}</p>
+	        <p className='links'><a href={this.props.github} className="btn btn-default" target="blank" role="button"><i className="fa fa-lg fa-github"></i></a><a href={this.props.url} className="btn btn-primary" disabled={this.props.disabled} target="blank" role="button"><i className="fa fa-globe fa-lg"></i></a></p>
+	      </div>
+	    </div>
+		</div>
+	)}
 }
 
 export default PortfolioItem
